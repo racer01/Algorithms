2436014c8b2d008fa8d0eda62ff05308b9955e89
László Székely-Tóth <racer01@users.noreply.github.com>
2017-09-20 14:17:39 +0200

    initial commit

---
+++ Algorithms.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Algorithms
+{
+    internal class Algorithms : IAlgorithms
+    {
+        public object[][] ChunkArrayInGroups(object[] numbers, int counter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ConfirmEnding(string word, string end)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object[] Destroyer(object[] numbers, params object[] obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long Factorialize(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int FindLongestWord(string word)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetIndexToIns(object[] numbers, float number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int[] LargestOfFour(int[,] numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Palindrome(string word)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RepeatStringNumTimes(string word, int counter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReverseString(string word)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Rot13(string word)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object[] Slasher(object[] numbers, int counter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string TitleCase(string word)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string TruncateString(string word, int counter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
